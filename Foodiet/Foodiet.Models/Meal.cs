d61d1db47a70808a08c82010d479df46b61bc066
Mihai <miumihai31@gmail.com>
2022-05-06 12:44:48 +0300

    Food Meal  Users FoodContext Created

---
+++ Foodiet/Foodiet.Models/Meal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Meal
+{
+	public Meal()
+	{
+		public int MealID { get; set; }
+		public string Meal_Name { get; set; }
+
+		public virtual ICollection<Food> Foods { get; set; }
+}
+}
